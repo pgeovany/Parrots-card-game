f8af6d20498478b385e90a31524c58eb3aaf1486
pgeovany <pgeovany@dcc.ufrj.br>
2022-04-07 17:33:49 -0300

    Implemented a function to populate the board according to the user input.

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,24 @@
 let numberOfCards = Number(prompt("Com quantas cartas deseja jogar?"));
-
-while(1) {
-   if(numberOfCards % 2 == 0 && numberOfCards >= 4 && numberOfCards <= 16){
-       break;
-   }
-   numberOfCards = Number(prompt("Com quantas cartas deseja jogar?"));
+while (1) {
+    if (numberOfCards % 2 == 0 && numberOfCards >= 4 && numberOfCards <= 16) {
+        populate();
+        break;
+    }
+    numberOfCards = Number(prompt("Com quantas cartas deseja jogar?"));
 }
 
-populate();
-
 function populate() {
-    
+    let board = document.querySelector(".board ul");
+
+    for (let i = 0; i < numberOfCards; i++) {
+        board.innerHTML += `
+        <li class="card">
+            <div class="card-front">
+                <img src="/images/front.png">
+            </div>
+            <div class="card-back">
+            </div>
+         </li>
+        `
+    }
 }
