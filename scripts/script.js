55febeb35ccb8b329f573f5d6a01ec7ec89545c2
pgeovany <pgeovany@dcc.ufrj.br>
2022-04-11 17:12:58 -0300

    Implemented a timer and fixed the bug where the user could break the game by clicking in several cards right before they were unflipped.

--- scripts/script.js
+++ scripts/script.js
@@ -4,18 +4,29 @@ let cardsBack = ["/images/bobrossparrot.gif", "/images/metalparrot.gif", "/image
 let flippedCards = [];
 let count = 0;
 let clicks = 0;
+let timer = -1;
+let idInterval  = setInterval(increaseTimer, 1000);
+
+function increaseTimer () {
+    timer++;
+    document.querySelector(".timer").innerHTML = timer;
+}
 
 cardsBack.sort(comparison);
-
 let numberOfCards = Number(prompt("Com quantas cartas você deseja jogar? (Insira um número par entre 4 e 14.)"));
 
-while (1) {
-    if (numberOfCards % 2 == 0 && numberOfCards >= 4 && numberOfCards <= 14) {
-        populate();
-        break;
+function startGame() {
+    while (1) {
+        if (numberOfCards % 2 == 0 && numberOfCards >= 4 && numberOfCards <= 14) {
+            populate();
+            break;
+        }
+        numberOfCards = Number(prompt("Com quantas cartas você deseja jogar? (Insira um número par entre 4 e 14.)"));
     }
-    numberOfCards = Number(prompt("Com quantas cartas deseja jogar?"));
 }
+
+startGame();
+increaseTimer();
 
 function comparison() {
     return Math.random() - 0.5;
@@ -66,26 +77,33 @@ function flip (element) {
             }
         }
         count++;
+        unclickable();
     }
 }
 
 function unflip() {
     flippedCards[flippedCards.length-2].element.querySelector(".front-face.face").classList.remove("flipped");
     flippedCards[flippedCards.length-2].element.querySelector(".back-face.face").classList.remove("flipped");
+    flippedCards[flippedCards.length-2].element.setAttribute("onclick", "flip(this)");
 
     flippedCards[flippedCards.length-1].element.querySelector(".front-face.face").classList.remove("flipped");
     flippedCards[flippedCards.length-1].element.querySelector(".back-face.face").classList.remove("flipped");
+    flippedCards[flippedCards.length-1].element.setAttribute("onclick", "flip(this)");
 
     flippedCards.length = flippedCards.length-2;
 }
 
 function gameOver() {
     if (flippedCards.length === numberOfCards) {
-        alert(`Parabéns, você venceu em ${clicks/2} jogadas!`);
-        let answer = prompt("Deseja jogar novamente?");
+        alert(`Parabéns! Você venceu em ${clicks} jogadas e em incríveis ${timer} segundos!`);
+        let answer = prompt("Deseja jogar novamente? ('sim' ou 'não')");
         if (answer === 'sim') {
             clear();
-            populate();
+            startGame();
+            idInterval  = setInterval(increaseTimer, 1000);
+        } else {
+            alert("Ok! Obrigado por jogar!");
+            clearInterval(idInterval);
         }
     }
 }
@@ -93,11 +111,14 @@ function gameOver() {
 function clear() {
     numberOfCards = Number(prompt("Com quantas cartas você deseja jogar? (Insira um número par entre 4 e 14.)"));
     document.querySelector(".board ul").innerHTML = "";
+    clearInterval(idInterval);
     flippedCards = [];
     clicks = 0;
+    timer = 0; 
+    document.querySelector(".timer").innerHTML = 0;
 }
 
-//Pairs that have already been flipped, won't call the 'flip' function on click anymore.
+//Cards that have already been flipped, won't call the 'flip' function on click anymore.
 function unclickable () {
     for(let i = 0; i < flippedCards.length; i++) {
         flippedCards[i].element.removeAttribute("onclick");
